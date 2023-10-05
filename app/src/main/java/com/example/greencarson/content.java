b6603002dc4218570384da21946bab55f6c50217
LFelipe06 <a01735939@tec.mx>
2023-10-05 10:45:21 -0600

    Added fragments. Map pending.

--- app/src/main/java/com/example/greencarson/content.java
+++ app/src/main/java/com/example/greencarson/content.java
@@ -1,14 +1,54 @@
 package com.example.greencarson;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class content extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class content extends AppCompatActivity{
+
+
+    BottomNavigationView bottomNavigationView;
+    NavigationFragment navigationFragment = new NavigationFragment();
+    AddCenterFragment addCenterFragment = new AddCenterFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+
+        /*SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);*/
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, navigationFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.navigation){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, navigationFragment).commit();
+                } else if (itemId == R.id.addCenter){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, addCenterFragment).commit();
+                } else if (itemId == R.id.profile){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                }
+
+                return true;
+            }
+        });
     }
 }
