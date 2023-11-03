dc1cb58c9339e07fbdc82550faefb8b3914968dc
OshkarVTec <a01275287@tec.mx>
2023-11-03 17:24:05 -0600

    Add login functionality

--- app/src/main/java/com/example/greencarson/MainActivity.java
+++ app/src/main/java/com/example/greencarson/MainActivity.java
@@ -1,20 +1,100 @@
 package com.example.greencarson;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Button login;
+    EditText email;
+    EditText password;
+    private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        email=findViewById(R.id.editTextEmail);
+        password=findViewById(R.id.editTextPassword);
+        login=findViewById(R.id.login);
+        db= FirebaseFirestore.getInstance();
     }
 
-    public void ingresar(View view){
-        startActivity(new Intent(this, content.class));
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            updateUI(currentUser);
+        }
+    }
+    private void signIn(View view, String email, String password) {
+        // [START sign_in_with_email]
+        if (Objects.equals(email, "") || Objects.equals(password, "")){
+            Log.w(TAG, "signInWithEmail:failure");
+            Snackbar.make(view, "Correo o contraseña incorrectos", Snackbar.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        assert user != null;
+                        updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Snackbar.make(view, "Correo o contraseña incorrectos", Snackbar.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+    private void updateUI(FirebaseUser user) {
+        String uid = user.getUid();
+        DocumentReference docRef = db.collection("administradores").document(uid);
+        docRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    Log.d(TAG, "Inicio de sesión exitoso");
+                    Intent home = new Intent(MainActivity.this, content.class);
+                    startActivity(home);
+                } else {
+                    Log.d(TAG, "El usuario no tiene permisos de administrador");
+                }
+            } else {
+                Log.d(TAG, "get failed with ", task.getException());
+            }
+        });
+    }
+    public void login(View v){
+        String emailText = email.getText().toString();
+        String passwordText = password.getText().toString();
+        this.signIn(v, emailText, passwordText);
     }
 }
